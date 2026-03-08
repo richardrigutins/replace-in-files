3dbb96a94c644baf8e67f0395ec6a543ae4e338a
richardrigutins <47950599+richardrigutins@users.noreply.github.com>
2026-03-08 17:39:46 +0100

    chore: configure rollup-plugin-license to generate license file

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,8 +1,12 @@
-// See: https://rollupjs.org/introduction/
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import license from 'rollup-plugin-license';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config = {
   input: 'src/main.ts',
@@ -12,7 +16,16 @@ const config = {
     format: 'es',
     sourcemap: true,
   },
-  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()],
+  plugins: [
+    typescript(),
+    nodeResolve({ preferBuiltins: true }),
+    commonjs(),
+    license({
+      thirdParty: {
+        output: path.join(__dirname, 'dist', 'licenses.txt'),
+      },
+    }),
+  ],
 };
 
 export default config;
